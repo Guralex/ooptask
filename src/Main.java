7ac84c0f7558217f15ccf40a002d9f46c6d75297
oleksandr_gura <Oleksandr_gura@epam.com>
2015-04-22 17:57:16 +0300

    Prepare for codding

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,12 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		int[] a = {1,2,5};
+		String s1= new String(a,1,2);
 
-		System.out.println("Hello 2");
+		
+		
+		System.out.println(s1);
 		
 	}
 
