a4c15d58f885971b27a6d16041795e4056111e5a
oleksandr_gura <Oleksandr_gura@epam.com>
2015-04-23 12:07:54 +0300

    12-07 Thu

---
+++ src/Plane.java
@@ -0,0 +1,36 @@
+
+public abstract class Plane {
+
+	
+	private int weightLimit;
+	private int passengerLimit;
+	private int fuelConsumption;
+	private int flightRange;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public int getWeightLimit() {
+		return weightLimit;
+	}
+	public int getPassengerLimit() {
+		return passengerLimit;
+	}
+	public int getFuelConsumption() {
+		return fuelConsumption;
+	}
+	public int getFlightRange() {
+		return flightRange;
+	}
+	
+}
